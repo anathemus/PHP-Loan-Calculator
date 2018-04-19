6f6caf1a3e3cd7af08b5bf7ff72fee349597d5b6
BenjaminABurgess <anathemus@gmail.com>
2018-04-18 21:40:20 -0400

    Beginning HTML Head
    Viewport, Initial Setup

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
+<html>
+    <head>
+        <meta author = "Benjamin A Burgess" />
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Benjamin A Burgess - Loan Calculator</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+        <script src="main.js"></script>
+    </head>
 
-?>
+    <?php
+
+    ?>
+    <body>
+    </body>
+</html>
